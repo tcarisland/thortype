0266124622320d1755d2798d66ae25537699a305
Thor Arisland <tcarisland@gmail.com>
2022-10-23 23:40:24 +0200

    #5 Font page stuff

--- pages/fonts/[font-page].tsx
+++ pages/fonts/[font-page].tsx
@@ -1,13 +1,16 @@
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 const FontPage: NextPage = () => {
     const router = useRouter()
-    const { pid } = router.query
+
+    const fontName = router.asPath.replace(/\/fonts\/(.*?)\//g, "$1");
 
     return(
         <div>
-            <h1> FontPage { pid } </h1>
+            <Image alt={fontName} src={"../../fonts/" + fontName + ".jpg"} width="600px" height="450px">
+            </Image>
         </div>
     )
 }
