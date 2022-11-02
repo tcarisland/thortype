17ffdee63c4788c6092be8abef14053f02c0ce94
Thor Arisland <tcarisland@gmail.com>
2022-11-02 01:45:07 +0100

    #5 Font Preview works

--- pages/fonts/[font-page].tsx
+++ pages/fonts/[font-page].tsx
@@ -9,9 +9,14 @@ const FontPage: NextPage = () => {
     const router = useRouter()
 
     const fontName = router.asPath.replace(/\/fonts\/(.*?)\//g, "$1");
+    const fontCssName = "tc-" + router.asPath.replace(/\/fonts\/(.*?)\//g, "$1");
 
     const font: Font | undefined = FontService.findFont(fontName);
-
+    const fontStyle = {
+        fontFamily: fontCssName,
+        fontSize: "24pt",
+        textAlign: "center"
+    }
     return(
         <div className="fontPageContainer">
             <Image alt={fontName} src={"../../fonts/" + fontName + ".jpg"} width="600px" height="450px">
@@ -19,8 +24,22 @@ const FontPage: NextPage = () => {
             { font !== undefined &&
                 <p>
                     <a href={ "/static/fonts/" + FontService.getFontFilePath(font)} download>
-                        { FontService.getFontFilePath(font) }
+                        Click here to download { font.name }
                     </a>
+                    <h3>
+                        Preview:
+                    </h3>
+                    <div style={fontStyle}>
+                        <p>
+                            THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG
+                        </p>
+                        <p>
+                            the quick brown fox jumps over the lazy dog
+                        </p>
+                        <p>
+                            0123456789
+                        </p>
+                    </div>
                 </p>
             }
         </div>
