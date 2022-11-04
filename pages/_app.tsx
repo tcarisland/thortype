2daf6d2b2e332159b8e059f67499792da7bb0929
Thor Arisland <tcarisland@gmail.com>
2022-11-05 00:00:25 +0100

    #11 added SSRProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import NavBar from '../components/navbar'
+import { SSRProvider } from 'react-bootstrap'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
+  return <SSRProvider>
     <NavBar></NavBar>
     <Component {...pageProps} />
-  </>
+  </SSRProvider>
 }
 
 export default MyApp
