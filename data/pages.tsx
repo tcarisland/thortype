41c9791b8129f7c4f864fa5e2b9e8394bcbae196
Thor Arisland <tcarisland@gmail.com>
2022-11-04 23:14:05 +0100

    #11 Created NavBar

---
+++ data/pages.tsx
@@ -0,0 +1,18 @@
+import { NavPage } from "../types/nav-page"
+
+const pages: Array<NavPage> = [
+    {
+        name: "Home",
+        link: "/"
+    },
+    {
+        name: "About",
+        link: "/about"
+    },
+    {
+        name: "Contact",
+        link: "/contact"
+    }
+];
+
+export default pages;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
-import Link from "next/link";
 import FontList from '../components/font-list';
+import NavBar from '../components/navbar';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const Home: NextPage = () => {
   return (
     <div>
+      <NavBar></NavBar>
       <FontList></FontList>
     </div>
 
