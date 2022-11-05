c5cbfcbaee29344ee1cc1752f1fb08edb21c488a
Thor Arisland <tcarisland@gmail.com>
2022-11-05 14:17:51 +0100

    #20 added validation to contact form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,48 +3,74 @@ import Script from "next/script";
 
 const Contact: NextPage = () => {
     return(
-        <div>
-            <Script src="./contact.js">            
-            </Script>
-                <div className="flex items-center min-h-screen bg-gray-900">
+        <div className=".text-gray-900">
+            <Script src="/static/scripts/contact.js" id="contactscript" strategy="beforeInteractive" defer={false}/>
+            <div className="flex items-center min-h-screen bg-gray-100 dark:bg-gray-900">
             <div className="container mx-auto">
-                <div className="max-w-md mx-auto my-10 bg-gray-800 p-5 rounded-md shadow-sm">
+                <div className="max-w-xl mx-auto my-10 p-5 rounded-md shadow-sm">
                 <div className="text-center">
-                    <h1 className="my-3 text-3xl font-semibold text-gray-100">
+                    <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-300">
                     Contact ThorType
                     </h1>
-                    <p className="text-gray-400 dark:text-gray-400">
-                    Fill up the form below.
+                    <p className="text-gray-600 dark:text-gray-300">
+                    Fill out the form below.
                     </p>
-                    <p>
-                    <a href="mailto:thor@thortype.com">If you're having trouble with the form, you can contact ThorType by sending an email here.</a>  
+                    <p className="text-gray-600 dark:text-gray-400">
+                    <a href="mailto:thor@thortype.com">If you&apos;re having trouble with the form, you can contact ThorType by sending an email here.</a>  
                     </p>
                 </div>
                 <div className="m-7">
-                    <form action="https://api.web3forms.com/submit" method="POST" id="form">
-                    <input type="hidden" name="access_key" value="f0915929-7a1a-4b0f-8706-96c9ba786e73"/>
-                    <input type="hidden" name="subject" value="New Submission from Web3Forms" />
+                    <form action="https://api.web3forms.com/submit" method="POST" id="form" className="needs-validation" noValidate>
+                    <input type="hidden" name="access_key" value="f0915929-7a1a-4b0f-8706-96c9ba786e73" />
+                    <input type="hidden" name="subject" value="" />
                     <input type="checkbox" name="botcheck" id="" style={{display: "none"}} />
 
-                    <div className="mb-6">
-                        <label htmlFor="name" className="block mb-2 text-sm text-gray-400">Full Name</label>
-                        <input type="text" name="name" id="name" placeholder="John Doe" required className="w-full px-3 py-2 h-12 rounded-sm placeholder-gray-500 text-gray-900 bg-gray-100 text-sm focus:outline-none" />
+                    <div className="flex mb-6 space-x-4">
+                        <div className="w-full md:w-1/2">
+                        <label htmlFor="fname" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">First Name</label>
+                        <input type="text" name="name" id="first_name" required className="w-full px-3 py-2 placeholder-gray-300 border-2 border-gray-200 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300" />
+                        <div className="empty-feedback invalid-feedback text-red-400 text-sm mt-1">
+                            Please provide your first name.
+                        </div>
+                        </div>
+                        <div className="w-full md:w-1/2">
+                        <label htmlFor="lname" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Last Name</label>
+                        <input type="text" name="last_name" id="lname" required className="w-full px-3 py-2 placeholder-gray-300 border-2 border-gray-200 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300" />
+                        <div className="empty-feedback invalid-feedback text-red-400 text-sm mt-1">
+                            Please provide your last name.
+                        </div>
+                        </div>
                     </div>
-                    <div className="mb-6">
-                        <label htmlFor="email" className="block mb-2 text-sm text-gray-400">Email Address</label>
-                        <input type="email" name="email" id="email" placeholder="you@company.com" required className="w-full px-3 py-2 h-12 rounded-sm placeholder-gray-500 text-gray-900 bg-gray-100 text-sm focus:outline-none" />
-                    </div>
-                    <div className="mb-6">
-                        <label htmlFor="phone" className="block mb-2 text-sm text-gray-400">Phone Number</label>
-                        <input type="text" name="phone" id="phone" placeholder="+1 (555) 1234-567" required className="w-full px-3 py-2 h-12 rounded-sm placeholder-gray-500 text-gray-900 bg-gray-100 text-sm focus:outline-none" />
-                    </div>
-                    <div className="mb-6">
-                        <label htmlFor="message" className="block mb-2 text-sm text-gray-400">Your Message</label>
 
-                        <textarea rows={5} name="message" id="message" placeholder="Your Message" className="w-full px-3 py-2 rounded-sm placeholder-gray-500 text-gray-900 bg-gray-100 text-sm focus:outline-none" required></textarea>
+                    <div className="flex mb-6 space-x-4">
+                        <div className="w-full md:w-1/2">
+                        <label htmlFor="email" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Email Address</label>
+                        <input type="email" name="email" id="email" required className="w-full px-3 py-2 placeholder-gray-300 border-2 border-gray-200 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300" />
+                        <div className="empty-feedback text-red-400 text-sm mt-1">
+                            Please provide your email address.
+                        </div>
+                        <div className="invalid-feedback text-red-400 text-sm mt-1">
+                            Please provide a valid email address.
+                        </div>
+                        </div>
+
+                        <div className="w-full md:w-1/2">
+
+                        <div className="empty-feedback invalid-feedback text-red-400 text-sm mt-1">
+                            Please provide your phone number.
+                        </div>
+                        </div>
                     </div>
                     <div className="mb-6">
-                        <button type="submit" className="w-full bg-indigo-600 inline-block text-white no-underline hover:text-indigo-100 py-4 px-4 rounded-sm focus:outline-none">
+                        <label htmlFor="message" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Your Message</label>
+
+                        <textarea rows={5} name="message" id="message" className="w-full px-3 py-2 placeholder-gray-300 border-2 border-gray-200 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300" required></textarea>
+                        <div className="empty-feedback invalid-feedback text-red-400 text-sm mt-1">
+                        Please enter your message.
+                        </div>
+                    </div>
+                    <div className="mb-6">
+                        <button type="submit" className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none">
                         Send Message
                         </button>
                     </div>
