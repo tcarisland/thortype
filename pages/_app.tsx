62331acc325a681e11071826353aeb24b97844ec
Thor Arisland <tcarisland@gmail.com>
2022-11-05 01:32:11 +0100

    #11 created a tailwind navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from '../components/layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div>
+    <Layout></Layout>
+    <Component {...pageProps} />
+    </div>
 }
 
 export default MyApp
---
+++ pages/contact.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Contact: NextPage = () => {
+    return(
+        <div>
+            Contact Page
+        </div>
+    )
+}
+
+export default Contact;
