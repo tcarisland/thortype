8ccf1134efc13fd0bb3847c9ac751efaa8d4d6d3
Thor Arisland <tcarisland@gmail.com>
2022-11-05 19:05:43 +0100

    #20 form styling

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -8,19 +8,26 @@ const Contact: NextPage = () => {
                 <input type="hidden" name="access_key" value="f0915929-7a1a-4b0f-8706-96c9ba786e73" />
                 <input type="hidden" name="subject" value="" />
                 <input type="checkbox" name="botcheck" id="" style={{ display: "none" }} />
+                <div className="text-center">
+                    <h2 className="text-2xl font-extrabold">Contact ThorType</h2>
+                </div>
+                <div className="text-center">
+                    <p>Fill out the form below.</p>
+                    <a href="mailto:thor@thortype.com">If you're having trouble with the form, you can contact ThorType by sending an email to thor@thortype.com</a>
+                </div>
                 <div>
-                    <label className="block" htmlFor="fname" >First Name</label>
-                    <input className="w-full" type="text" name="name" id="first_name" required />
-                    <label className="block" htmlFor="lname" >Last Name</label>
-                    <input className="w-full" type="text" name="last_name" id="lname" required />
+                    <label className="block mb-2 text-sm text-gray-600" htmlFor="fname" >First Name</label>
+                    <input className="w-full px-3 py-2 h-12 rounded-sm placeholder-gray-500 text-gray-900 bg-gray-100 text-sm focus:outline-none" type="text" name="name" id="first_name" required />
+                    <label className="block mb-2 text-sm text-gray-600" htmlFor="lname" >Last Name</label>
+                    <input className="w-full px-3 py-2 h-12 rounded-sm placeholder-gray-500 text-gray-900 bg-gray-100 text-sm focus:outline-none" type="text" name="last_name" id="lname" required />
                 </div>
                 <div >
-                    <label className="block" htmlFor="email" >Email Address</label>
-                    <input className="w-full" type="email" name="email" id="email" required />
+                    <label className="block mb-2 text-sm text-gray-600" htmlFor="email" >Email Address</label>
+                    <input className="w-full px-3 py-2 rounded-sm placeholder-gray-500 text-gray-900 bg-gray-100 text-sm focus:outline-none" type="email" name="email" id="email" required />
                 </div>
                 <div >
-                    <label className="block" htmlFor="message" >Your Message</label>
-                    <textarea rows={5} className="w-full" name="message" id="message" required></textarea>
+                    <label className="block mb-2 text-sm text-gray-600" htmlFor="message" >Your Message</label>
+                    <textarea rows={5} className="w-full px-3 py-2 rounded-sm placeholder-gray-500 text-gray-900 bg-gray-100 text-sm focus:outline-none" name="message" id="message" required></textarea>
                 </div>
                 <div >
                     <button className="w-full bg-indigo-600 inline-block text-white no-underline hover:text-indigo-100 py-4 px-4 rounded-sm focus:outline-none" type="submit" >Send Message</button>
