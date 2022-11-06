54c38c6050a6e1799087f5b2fa32b92a14735004
Thor Arisland <tcarisland@gmail.com>
2022-11-06 15:56:02 +0100

    #16 #23 replaced fontlist

--- services/font-service.ts
+++ services/font-service.ts
@@ -1,11 +1,12 @@
 import FontFileSuffix from "../model/font-file-suffix";
 import { FontStandard } from "../model/font-standard";
 import Font from "../model/font";
+import fonts from "../data/font-list";
 
 export default class FontService {
 
     static get fontList(): string[] {
-        return fontList.map(font => font.name);
+        return fonts.map(font => font.name);
     }
 
     static get fontFileSuffixes(): FontFileSuffix[] {
@@ -13,7 +14,7 @@ export default class FontService {
     }
 
     public static findFont(name: string): Font | undefined {
-        return fontList.find(font => font.name === name);
+        return fonts.find(font => font.name === name);
     }
 
     public static getFontFilePath(font: Font) {
@@ -26,42 +27,3 @@ const fontFileSuffixes: FontFileSuffix[] = [
     { type: FontStandard.OpenType, suffix: ".otf" },
     { type: FontStandard.TrueType, suffix: ".ttf" }
 ]
-
-const fontList: Font[] = [
-    { "name": "pipeline", "type": FontStandard.OpenType },
-    { "name": "tuscan-modular", "type": FontStandard.OpenType },
-    { "name": "16-segment-display", "type": FontStandard.OpenType },
-    { "name": "cardboard-cutout", "type": FontStandard.OpenType },
-    { "name": "demodulator", "type": FontStandard.TrueType},
-    { "name": "anderson", "type": FontStandard.TrueType },
-    { "name": "belleview", "type": FontStandard.OpenType },
-    { "name": "betong", "type": FontStandard.TrueType },
-    { "name": "borgen", "type": FontStandard.OpenType },
-    { "name": "brush-off", "type": FontStandard.TrueType },
-    { "name": "buckley-junior", "type": FontStandard.OpenType },
-    { "name": "caroline", "type": FontStandard.TrueType },
-    { "name": "college-sans", "type": FontStandard.TrueType },
-    { "name": "dm80", "type": FontStandard.OpenType },
-    { "name": "giovanni", "type": FontStandard.TrueType },
-    { "name": "hairline", "type": FontStandard.OpenType },
-    { "name": "high-society", "type": FontStandard.TrueType },
-    { "name": "hothead", "type": FontStandard.TrueType },
-    { "name": "i8080", "type": FontStandard.TrueType },
-    { "name": "inverted-stencil", "type": FontStandard.TrueType },
-    { "name": "klub-katz", "type": FontStandard.TrueType },
-    { "name": "manos", "type": FontStandard.TrueType },
-    { "name": "metal-plate", "type": FontStandard.OpenType },
-    { "name": "modum", "type": FontStandard.TrueType },
-    { "name": "monomod", "type": FontStandard.TrueType },
-    { "name": "ortho-graphix", "type": FontStandard.OpenType },
-    { "name": "ragtime", "type": FontStandard.OpenType },
-    { "name": "roland", "type": FontStandard.TrueType },
-    { "name": "salome", "type": FontStandard.TrueType },
-    { "name": "snufkin", "type": FontStandard.OpenType },
-    { "name": "solid-sans", "type": FontStandard.TrueType },
-    { "name": "sullivan", "type": FontStandard.TrueType },
-    { "name": "taylor-gothic", "type": FontStandard.OpenType },
-    { "name": "taylor", "type": FontStandard.OpenType },
-    { "name": "tuscan-black", "type": FontStandard.OpenType },
-    { "name": "zx80", "type": FontStandard.TrueType }
-];
