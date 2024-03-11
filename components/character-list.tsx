ca570d6dd776995b41474eacd3e4b12d82b7c5ae
tcarisland <tcarisland@gmail.com>
2024-03-11 19:41:45 +0100

    added info for each glyph closes #26

--- components/character-list.tsx
+++ components/character-list.tsx
@@ -1,5 +1,6 @@
 import React, {ReactElement, SyntheticEvent, useState} from 'react';
 import { Font } from '../data/font';
+import {fontFamily} from "@mui/system";
 
 export interface CharacterListProps {
     fontFilePath: string,
@@ -16,7 +17,6 @@ export interface CharacterListState {
 }
 
 export default class CharacterList extends React.Component<CharacterListProps, CharacterListState> {
-    i = 0;
     fontCssName = "tc-" + this.props.fontName.replace(/\/fonts\/(.*?)\//g, "$1");
     glyphSize = 100
     padding= 20
@@ -67,20 +67,29 @@ export default class CharacterList extends React.Component<CharacterListProps, C
 
     renderGlyph(glyph: number, i: number): ReactElement<any, any> {
       let unicodeString = String.fromCharCode(glyph);
-      return <div style={{
-        border: "1px solid black",
-        width: this.glyphSize + "px",
-        height: this.glyphSize + "px",
-        lineHeight: "80px",
-        textAlign: "center"}} key={"glyphPreviewBox" + i}>
-        { unicodeString }
+      return <div style={{border: "2px solid #AAA", borderRadius: "5px"}}>
+        <div className="ml-1" style={{fontWeight: "bold", fontSize: "12px", fontFamily: "Geneva, sans-serif"}}>
+          {
+            i + " " + unicodeString + " " + glyph + " " + glyph.toString(16).padStart(4, "0")
+          }
+        </div>
+        <div style={{
+          fontFamily: this.state.fontFamily,
+          fontSize: this.state.fontSize + "px",
+          width: this.glyphSize + "px",
+          height: this.glyphSize + "px",
+          lineHeight: "80px",
+          textAlign: "center"
+        }} key={"glyphPreviewBox" + i}>
+          {unicodeString}
+        </div>
       </div>
     }
 
   isCharacterListNotEmpty() {
-      return this.props.font &&
-        this.props.font.characterMap &&
-        this.props.font.characterMap.subtables[0] &&
+    return this.props.font &&
+      this.props.font.characterMap &&
+      this.props.font.characterMap.subtables[0] &&
         this.props.font.characterMap.subtables[0].characterList &&
         this.props.font.characterMap.subtables[0].characterList.length
   }
@@ -108,20 +117,12 @@ export default class CharacterList extends React.Component<CharacterListProps, C
                 </p>
               </div>
             }
-            <div className='grid justify-between gap-2'
-                 style={{
-                   width: this.state.width + "px",
-                   height: this.state.height + "px",
-                   fontFamily: this.state.fontFamily,
-                   fontSize: this.state.fontSize + "px",
-                   lineHeight: this.state.fontSize + "px",
-                   gridTemplateColumns: this.state.gridTemplateColumns
-                 }}>
+            <div className='grid justify-between gap-2' style={{gridTemplateColumns: this.state.gridTemplateColumns}}>
               {
                 this.isCharacterListNotEmpty() &&
                 this.props.font.characterMap.subtables[0].characterList.map(
-                  characterIndex => {
-                    return this.renderGlyph(characterIndex, this.i++)
+                  (characterIndex, index) => {
+                    return this.renderGlyph(characterIndex, index)
                   }
                 )
               }
