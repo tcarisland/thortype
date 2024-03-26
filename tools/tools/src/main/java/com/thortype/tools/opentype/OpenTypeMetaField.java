cce0fbd1b2d2d1040436dafdff1861fa257cd755
tcarisland <tcarisland@gmail.com>
2024-03-26 21:19:31 +0100

    removed tools now that tsfontconverter works closes #53

--- tools/tools/src/main/java/com/thortype/tools/opentype/OpenTypeMetaField.java
+++
@@ -1,9 +0,0 @@
-package com.thortype.tools.opentype;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface OpenTypeMetaField {
-    public OpenTypeNameTableName value() default OpenTypeNameTableName.INVALID;
-}
--- tools/tools/src/main/java/com/thortype/tools/typescript/FontTsxField.java
+++
@@ -1,9 +0,0 @@
-package com.thortype.tools.typescript;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface FontTsxField {
-    public String value() default "string";
-}
